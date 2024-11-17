86bdcdfa37167a69ee2cc9abbc64712857c55cde
breinergg <bmgonzalez@unimagdalena.edu.co>
2024-11-17 10:21:49 -0500

    primer commit

---
+++ src/data/task.js
@@ -0,0 +1,22 @@
+export const tasks = [
+    {
+        id: 1,
+        title: 'Task 1',
+        description: 'Primera tarea'
+    },
+    {
+        id: 2,
+        title: 'Task 2',
+        description: 'Segunda tarea'
+    },
+    {
+        id: 3,
+        title: 'Task 3',
+        description: 'Tercera tarea'
+    },
+    {
+        id: 4,
+        title: 'Task 4',
+        description: 'Cuarta tarea'
+    }
+];
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {TaskContextProvider} from './context/TaskContext.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TaskContextProvider>
+      <App />
+    </TaskContextProvider>
+  </React.StrictMode>
+)
